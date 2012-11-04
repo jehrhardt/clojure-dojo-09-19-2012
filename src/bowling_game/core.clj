d42527f20a5993e525e268be152724cdb73f22e6
Jan Ehrhardt <jan.ehrhardt@googlemail.com>
2012-11-04 16:34:50 +0100

    Results from 1th November added

--- src/bowling_game/core.clj
+++ src/bowling_game/core.clj
@@ -1,7 +1,16 @@
 (ns bowling-game.core)
 
+(defn frame [game]
+  ((fn [myseq restgame]
+    (if restgame
+    (recur 
+       (conj myseq (seq [(first restgame) (second restgame)])) 
+       (nnext restgame))
+     myseq))
+  (seq []) game))
+
 (defn score [game]
   ((fn [sum list]
-    (if (empty? list)
-      sum
-      (recur (+ sum (first list)) (rest list)))) 0 game))
+     (if (empty? list)
+       sum
+       (recur (+ sum (first list)) (rest list)))) 0 game))
--- test/bowling_game/core_test.clj
+++ test/bowling_game/core_test.clj
@@ -5,4 +5,8 @@
 
 (fact (score (repeat 20 0)) => 0)
 
+(fact (frame (repeat 10 0)) => (just ((0 0) (0 0) (0 0) (0 0) (0 0))))
+
 (fact (score (repeat 20 4)) => 80)
+
+(fact (score (repeat 12 10)) => 300)
